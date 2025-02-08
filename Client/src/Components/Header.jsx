1b8e85948abe3884ac604067bd69755836c51b11
Stanley Odhiambomwai <odhiambostanley799@gmail.com>
2025-02-08 18:16:22 +0300

    adding A header component

---
+++ Client/src/Components/Header.jsx
@@ -0,0 +1,27 @@
+// src/components/Header.jsx
+import { Link } from "react-router";
+
+const Header = ({ heading, paragraph, linkName, linkUrl }) => {
+  return (
+    <div className="mb-10">
+      <div className="flex justify-center">
+        <img
+          alt="Logo"
+          className="h-14 w-14"
+          src="https://ik.imagekit.io/pibjyepn7p9/Lilac_Navy_Simple_Line_Business_Logo_CGktk8RHK.png?ik-sdk-version=javascript-1.4.3&updatedAt=1649962071315"
+        />
+      </div>
+      <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+        {heading}
+      </h2>
+      <p className="mt-5 text-center text-sm text-gray-600">
+        {paragraph}{" "}
+        <Link to={linkUrl} className="font-medium text-purple-600 hover:text-purple-500">
+          {linkName}
+        </Link>
+      </p>
+    </div>
+  );
+};
+
+export default Header;
