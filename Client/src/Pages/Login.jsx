745c94b25db9ea112967646e842e026e515dcc18
Stanley Odhiambomwai <odhiambostanley799@gmail.com>
2025-02-08 15:21:24 +0300

    commiting the login page

---
+++ Client/src/Pages/Login.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "../components/Header";
+import Login from "../components/Login";
+
+export default function LoginPage() {
+  return (
+    <>
+      <Header
+        heading="Login to your account"
+        paragraph="Don't have an account yet? "
+        linkName="Register"
+        linkUrl="/register"
+      />
+      <Login />
+    </>
+  );
+}
