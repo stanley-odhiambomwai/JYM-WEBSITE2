942748b9b21951d7993ee1148ac3a61500d6fe85
Stanley Odhiambomwai <odhiambostanley799@gmail.com>
2025-02-11 09:32:35 +0300

    importing cookies in the server.js file

--- Server/Src/Server.js
+++ Server/Src/Server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv"
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 const app = express();
@@ -9,6 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("JYM Backend API is running");
